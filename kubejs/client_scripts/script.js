b14c74fed82ba5a7f01ca284b7d0f3a5bd5c59ad
MrExpert <135133933+MrExpertGitHub@users.noreply.github.com>
2023-10-12 21:14:12 +0200

    Added new ores
    There are might be some bugs

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -6,3 +6,15 @@ onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
 })
+
+//₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₊ ₋ ₌ ₍ ₎ ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾
+
+onEvent('item.tooltip', tooltip => {
+	// §9 => solid
+	// §e => liquid
+	// §7 => gas
+	// §d => super-critical
+	tooltip.add(['minecraft:iron_ore', 'minecraft:deepslate_iron_ore'], '§9Fe₃O₄')
+	tooltip.add(['kubejs:deepsleate_pyrite_ore', 'kubejs:pyrite_ore'], '§9FeS₂')
+	tooltip.add(['minecraft:copper_ore', 'minecraft:deepslate_copper_ore'], '§9Cu₂CO₃(OH)₂')
+  })
